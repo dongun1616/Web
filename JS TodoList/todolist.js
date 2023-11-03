0915fe4b23df37ed2298216ef3eca8d1eddc1708
dongun1616 <dongun16@naver.com>
2023-11-03 22:12:14 +0900

    2023.11.03-js-1st

--- JS TodoList/todolist.js
+++ JS TodoList/todolist.js
@@ -1,14 +1,33 @@
 // todo list
 
-let question = prompt("What would you to do?");
-while (!question) {
-    question = prompt("please what would you to do???");
-}
+let question = prompt("What would you to do? (Type 'quit' to quit)");
+const listArray = [];
+while (question !== "quit") {
 
-if (question === "new") {
-    let list = prompt("Add a todo");
-    console.log(list);
+    if (question === "new") {
+        question = prompt("enter a todo list?");
+        listArray.push(question);
+        console.log(`${question} added to list`);
+    }
+    else if (question === "list") {
+        console.log("**********")
+        for (let i = 0; i < listArray.length; i++) {
+            console.log(`${i} : ${listArray[i]}`);
+        }
+        console.log("**********")
+    }
+    else if (question === "delete") {
+        question = parseInt(prompt("enter index of todo to delete"));
+        if (!Number.isNaN(question)) {
+            const deleted = listArray.splice(question, 1);
+            console.log(`ok, deleted ${deleted}`);
+        }
+        else {
+            console.log("Unknown index");
+        }
+    }
+    question = prompt("What would you to do? (Type 'quit' to quit)");
 }
-else if (question === "list") {
-
+if (question === "quit") {
+    console.log("ok, you quit the list");
 }
