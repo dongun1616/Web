d55eabe0ca212661b503a382a10043d7146581bd
dongun1616 <dongun16@naver.com>
2023-11-08 21:05:41 +0900

    2023.11.08-js-1st

---
+++ JS Function2/jsfunction2.js
@@ -0,0 +1,16 @@
+function collectEggs() {
+    let totalEggs = 6;
+    console.log(totalEggs);
+}
+
+
+
+// 기본적인 함수
+function add(x, y) {
+    return x + y;
+}
+
+// 함수 표현식
+const add = function (x, y) {
+    return x + y;
+}
