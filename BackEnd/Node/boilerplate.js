ec4dfb7164c5af0e925820a8458ddd0b4eb62ce1
dongun1616 <dongun16@naver.com>
2023-12-14 23:47:43 +0900

    2023.12.14-1st

---
+++ BackEnd/Node/boilerplate.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const folderName = process.argv[2] || 'Project';
+
+try {
+    fs.mkdirSync(folderName);
+    fs.writeFileSync(`${folderName}/index.html`)
+    fs.writeFileSync(`${folderName}/app.js`)
+    fs.writeFileSync(`${folderName}/style.css`)
+} catch (e) {
+    console.log("error!!")
+    console.log(e);
+}
