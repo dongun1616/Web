6f3a1f1ab2df0c1ce699eb1b6fbcd7c2f2f3c0ab
dongun1616 <dongun16@naver.com>
2024-01-01 21:49:51 +0900

    2024.01.01-1st

--- BackEnd/Mongoose/product.js
+++ BackEnd/Mongoose/product.js
@@ -31,15 +31,33 @@ const productSchema = new mongoose.Schema({
     }
 })
 
+productSchema.methods.toggleOnSale = function () {
+    this.onSale = !this.onSale;
+    return this.save()
+}
+
+// productSchema.statics.fireSale=function(){
+//     //code
+// }
+
 const Product = mongoose.model('Product', productSchema);
 
-const bike = new Product({ name: 'Bike2', price: 299, categories: ['Cycling', 'Safety'], qty: { online: 4 }, size: 'M' });
-bike.save()
-    .then(data => {
-        console.log("It worked!!")
-        console.log(data)
-    })
-    .catch(err => {
-        console.log("oh no error!!")
-        console.log(err)
-    })
+const findProduct = async () => {
+    const foundProduct = await Product.findOne({ name: 'Mountain Bike' })
+    console.log(foundProduct)
+    await foundProduct.toggleOnSale();
+    console.log(foundProduct)
+}
+
+findProduct();
+
+// const bike = new Product({ name: 'Bike2', price: 299, categories: ['Cycling', 'Safety'], qty: { online: 4 }, size: 'M' });
+// bike.save()
+//     .then(data => {
+//         console.log("It worked!!")
+//         console.log(data)
+//     })
+//     .catch(err => {
+//         console.log("oh no error!!")
+//         console.log(err)
+//     })
