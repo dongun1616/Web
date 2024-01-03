b5ba2f42fc9dab45d2815e6b589dfa160d805de1
dongun1616 <dongun16@naver.com>
2024-01-03 19:40:01 +0900

    2024.01.03-1st

--- BackEnd/Mongoose_Express/index.js
+++ BackEnd/Mongoose_Express/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override')
 
 const Product = require('./models/product');
 
@@ -14,11 +15,49 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
         console.log(err);
     })
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('views engine', 'ejs');
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
-app.get('/dog', (req, res) => {
-    res.send('Woof!');
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.get('/products', async (req, res) => {
+    const allProduct = await Product.find({})
+    res.render('products/index', { allProduct })
+})
+
+app.get('/products/new', (req, res) => {
+    res.render('products/new')
+})
+
+app.post('/products', async (req, res) => {
+    const newProduct = new Product(req.body)
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`)
+})
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id)
+    res.render('products/show', { product })
+})
+
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id)
+    res.render('products/edit', { product })
+})
+
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
+    res.redirect(`/products/${product._id}`)
+})
+
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const deleteProduct = await Product.findByIdAndDelete(id)
+    res.redirect('/products')
 })
 
 app.listen(3000, () => {
