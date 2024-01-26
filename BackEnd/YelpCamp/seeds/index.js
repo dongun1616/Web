d005e6337c8b02f0c6ef6eaf8420319e7af03381
dongun1616 <dongun16@naver.com>
2024-01-26 19:57:22 +0900

    2024.01.26-1st-yelpcamp-bootstrap

--- BackEnd/YelpCamp/seeds/index.js
+++ BackEnd/YelpCamp/seeds/index.js
@@ -18,9 +18,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});  // 모든 캠핑장 삭제후 for루프 실행
     for (let i = 0; i < 50; i++) {   //50개의 랜덤한 캠핑장 생성
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat repellendus deleniti dolorem provident neque a magnam numquam non, maiores facilis enim hic quae veritatis qui, consequuntur quo laborum unde omnis!',
+            price
         })
         await camp.save();
     }
