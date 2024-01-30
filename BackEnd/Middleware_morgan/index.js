d62f6990366cd2629999d4f697ed5cbb05290d6e
dongun1616 <dongun16@naver.com>
2024-01-30 21:33:11 +0900

    2024.01.30-1st-Express-error

--- BackEnd/Middleware_morgan/index.js
+++ BackEnd/Middleware_morgan/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
+const AppError = require('./AppError');  //AppError 가져오기
+
 app.use(morgan('tiny'));
 
 app.use((req, res, next) => {
@@ -11,33 +13,61 @@ app.use((req, res, next) => {
 })
 
 app.use('/dogs', (req, res, next) => {
-    console.log("i love dogs!");
+    console.log("I LOVE DOGS!!")
     next();
 })
 
-const verifyPassword = ((req, res, next) => {
+const verifyPassword = (req, res, next) => {
     const { password } = req.query;
     if (password === 'chickennugget') {
         next();
     }
-    res.send('Sorry you need a password')
-})
+    throw new AppError('password required', 401);
+    // res.send("PASSWORD NEEDED!")
+    // throw new AppError('Password required!', 400)
+}
 
 app.get('/', (req, res) => {
-    res.send('Home page')
+    console.log(`REQUEST DATE: ${req.requestTime}`)
+    res.send('HOME PAGE!')
 })
+
+app.get('/error', (req, res) => {
+    chicken.fly()
+})
+
 app.get('/dogs', (req, res) => {
-    res.send('Woof Woof')
+    console.log(`REQUEST DATE: ${req.requestTime}`)
+    res.send('WOOF WOOF!')
 })
 
 app.get('/secret', verifyPassword, (req, res) => {
-    res.send('my secret is : cool')
+    res.send('MY SECRET IS: Sometimes I wear headphones in public so I dont have to talk to anyone')
+})
+
+app.get('/admin', (req, res) => {
+    throw new AppError('You are not an Admin!', 403)
 })
 
 app.use((req, res) => {
-    res.status(404).send("not found")  //마지막 app.use 모든 미들웨어가 지난후 실행해 찾을수 없다는걸 알림
+    res.status(404).send('NOT FOUND!')
 })
 
+
+// app.use((err, req, res, next) => {
+//     console.log("******************************************")
+//     console.log("*****************ERROR*****************")
+//     console.log("******************************************")
+//     console.log(err)
+//     next(err)
+// })
+
+app.use((err, req, res, next) => {
+    const { status = 500, message = 'Something went wrong' } = err;
+    res.status(status).send(message)
+})
+
+
 app.listen(3000, () => {
-    console.log("App is running on 3000 port");
+    console.log('App is running on localhost:3000')
 })
