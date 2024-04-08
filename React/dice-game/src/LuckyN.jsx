99e2c1c07c50c90ee5ff5303ce8ef1c9fe542b09
dongun0116 <dongun16@naver.com>
2024-04-08 21:37:16 +0900

    error_commit

--- React/dice-game/src/LuckyN.jsx
+++ React/dice-game/src/LuckyN.jsx
@@ -5,8 +5,8 @@ import Dice from "./Dice";
 
 export default function LuckyN({ numDice = 2, goal = 7 }) {
     const [dice, setDice] = useState(getRolls(numDice))
-    const isWinner = sum(dice) === goal
-    const roll = () => {
+    const isWinner = sum(dice) === goal //더한 주사위값이 목표값과 같은지
+    const roll = () => { //주사위를 다시 돌리는 함수
         setDice((getRolls(numDice)))
     }
     return (
