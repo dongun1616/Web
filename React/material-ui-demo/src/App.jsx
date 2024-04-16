cd481969b1118eedebbe430fff3a4b5d6bf73815
dongun0116 <dongun16@naver.com>
2024-04-16 23:26:44 +0900

    2024.04.16-1st-react_Material_UI

---
+++ React/material-ui-demo/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import RatingDemo from './RatingDemo';
+import FormDemo from './FormDemo';
+import NavBar from './NavBar';
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+      <FormDemo />
+    </>
+  )
+}
+
+export default App
